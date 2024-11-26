a77889202f950cb9f59b9d3206523bfd51dbf15a
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-11-26 21:16:18 +0100

    change @sveltejs/adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { VitePWA } from 'vite-plugin-pwa';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
