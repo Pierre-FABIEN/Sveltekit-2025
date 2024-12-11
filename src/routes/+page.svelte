7d1f48f89f7353c03d57785aef0844309e34e170
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-12-11 17:10:35 +0100

    blog wip clear

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,5 @@
-<<<<<<< Updated upstream
-<script>
-	import { Button } from '$lib/components/shadcn/ui/button';
-</script>
-
-<section class="flex min-h-screen items-center justify-center">
-	<div class="container text-center">
-		<!-- Header -->
-		<h1 class="text-5xl font-extrabold leading-tight sm:text-6xl">
-			Bienvenue sur le <br /><span class="">Boilerplate SvelteKit 2025</span>
-		</h1>
-		<p class="mt-4 text-lg sm:text-xl">
-			Accélérez le développement de vos applications avec un setup moderne et performant.
-		</p>
-		<!-- Action Buttons -->
-		<div class="mt-8 flex justify-center space-x-4">
-			<Button href="/home/presentation" variant="default" size="lg">Présentation</Button>
-			<Button
-				href="https://github.com/Pierre-FABIEN/Sveltekit-2025"
-				target="_blank"
-				variant="secondary"
-				size="lg"
-			>
-				Voir sur GitHub
-			</Button>
-=======
 <script lang="ts">
+	import Button from '$shadcn/button/button.svelte';
 	import { toast } from 'svelte-sonner';
 
 	// Function to copy text content to clipboard
@@ -131,7 +106,6 @@
 			<div class="mt-8 flex justify-center space-x-4">
 				<a href="/presentation" class="presentationButton"> Présentation </a>
 			</div>
->>>>>>> Stashed changes
 		</div>
 	</div>
 </div>
